883a3e0602140729b8b6bd0ecc3770c9196082e5
Ashley Ng <ashley.ng@utexas.edu>
2015-07-30 14:40:54 -0500

    add base statistics functions

---
+++ statistics.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include "statistics.h"
+#include <opencv2/objdetect/objdetect.hpp>
+#include <numeric>
+
+using namespace cv;
+using namespace std;
+
+void percent_error(Point actual, Point measured, int pixels_x, int pixels_y) {
+    double error_x = abs((actual.x/pixels_x) - (measured.x/pixels_x)) /
+            (actual.x/pixels_x);
+    double error_y = abs((actual.y/pixels_y) - (measured.y/pixels_y)) /
+            (actual.y/pixels_y);
+}
+
+Point averge_distance(vector<Point> measured_points) {
+    double sum_x, sum_y;
+    for(int x = 0; x < measured_points.size(); x++) {
+        sum_x += measured_points[x].x;
+        sum_y += measured_points[x].y;
+    }
+    return Point(sum_x/measured_points.size(), sum_y/measured_points.size());
+}
