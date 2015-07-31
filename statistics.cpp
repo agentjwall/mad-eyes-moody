c7be52e8ed23947a74b58e7d91b14bf64212e9e4
Ashley Ng <ashley.ng@utexas.edu>
2015-07-30 21:21:20 -0500

    add is_hit functions

--- statistics.cpp
+++ statistics.cpp
@@ -40,25 +40,82 @@ vector<string> split(const string &s, char delim) {
 
 int const num_data = 20;
 
+vector<int> to_int(vector<string> string_vector) {
+    vector<int> int_vector;
+    for (int x = 0; x < string_vector.size(); x++) {
+        int_vector.push_back(stoi(string_vector[x]));
+    }
+    return int_vector;
+}
+/*
+ * tests to see if the actual sphere is
+ * the same as expected sphere
+ * return true if it's a hit, false if it's a miss
+ *
+ * returns: true or false
+ */
+bool is_hit(vector<int> data) {
+    assert(data.size() == 6);
+
+    if (data[0] == data[2] && data[1] == data[3]) {
+        return true;
+    }
+    return false;
+}
+
+
+/*
+ * tests to see if the x value of the actual sphere
+ * is the same as the x value of the expected sphere
+ * return true if it's a hit, false if it's a miss
+ *
+ * returns: true or false
+ */
+bool is_x_hit(vector<int> data) {
+    assert(data.size() == 6);
+
+    if (data[0] == data[2]) {
+        return true;
+    }
+    return false;
+}
+
+/*
+ * tests to see if the y value of the actual sphere
+ * is the same as the y value of the expected sphere
+ * return true if it's a hit, false if it's a miss
+ *
+ * returns: true or false
+ */
+bool is_y_hit(vector<int> data) {
+    assert(data.size() == 6);
+
+    if (data[1] == data[3]) {
+        return true;
+    }
+    return false;
+}
+
 int main(int argc, char* argv[]) {
 
-    ifstream myfile("percent_error_data1.txt");
+    ifstream myfile("percent_error_data2.txt");
     string line;
     int hits = 0;
-    int hits_x = 0;
     int misses = 0;
+
+    int hits_x = 0;
     int misses_x = 0;
+
     int hits_y = 0;
     int misses_y = 0;
+
     int sum_x = 0;
     int sum_y = 0;
+
     while (getline(myfile, line)) {
         if (line[0] != 'R') {
             vector<string> data = split(line, ',');
-            for (int x = 0; x < data.size(); x++) {
-                cout << data[x] << "-";
-            }
-            cout << endl;
+            vector<int> numbers = to_int(data);
         }
         //reset
         else {
