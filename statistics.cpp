7cc7f625048d0be8332bb90bb8708b29882d0d9a
Ashley Ng <ashley.ng@utexas.edu>
2015-07-30 21:49:18 -0500

    out put all data for hits and misses

--- statistics.cpp
+++ statistics.cpp
@@ -14,6 +14,7 @@ void percent_error(Point actual, Point measured, int pixels_x, int pixels_y) {
             (actual.y/pixels_y);
 }
 
+
 Point averge_distance(vector<Point> measured_points) {
     double sum_x, sum_y;
     for(int x = 0; x < measured_points.size(); x++) {
@@ -38,7 +39,7 @@ vector<string> split(const string &s, char delim) {
     return elems;
 }
 
-int const num_data = 20;
+int const num_data = 19;
 
 vector<int> to_int(vector<string> string_vector) {
     vector<int> int_vector;
@@ -112,18 +113,74 @@ int main(int argc, char* argv[]) {
     int sum_x = 0;
     int sum_y = 0;
 
+    int count = 0;
+    string line_number = "0";
+    Point expected;
+
     while (getline(myfile, line)) {
         if (line[0] != 'R') {
             vector<string> data = split(line, ',');
             vector<int> numbers = to_int(data);
+            expected = Point(numbers[0], numbers[1]);
+
+            sum_x += numbers[4];
+            sum_y += numbers[5];
+
+            count++;
+
+            if(is_hit(numbers)) {
+                hits++;
+            }
+            else {
+                misses++;
+            }
+
+            if (is_x_hit(numbers)) {
+                hits_x++;
+            }
+            else {
+                misses_x++;
+            }
+
+            if (is_y_hit(numbers)) {
+                hits_y++;
+            }
+            else {
+                misses_y++;
+            }
         }
         //reset
         else {
-            if (hits + misses + sum_x + sum_y > 0) {
-                cout << "print percent error" << endl;
+            if (count == num_data) {
+                cout << "regions " << line_number << " data" << endl;
+                cout << "hits: " << hits << " misses: " << misses << " PERCENT ERROR: " << endl;
+                cout << "hits on x-axis: " << hits_x << " misses on x-axis: " << misses_x << endl;
+                cout << "hits on y-axis: " << hits_y << " misses on y-axis: " << misses_y << endl;
+                cout << "average guess point: " << sum_x/num_data << ", " << sum_y/num_data << endl;
+                cout << "expected point: " << expected.x << ", " << expected.y << endl;
+
+                hits = 0;
+                misses = 0;
+                hits_x = 0;
+                misses_x = 0;
+                hits_y = 0;
+                misses_y = 0;
+                sum_x = 0;
+                sum_y = 0;
+                count = 0;
             }
+            line_number = line[line.size() - 1];
         }
+
+
     }
+    cout << "regions " << line_number << " data" << endl;
+    cout << "hits: " << hits << " misses: " << misses << endl;
+    cout << "hits on x-axis: " << hits_x << " misses on x-axis: " << misses_x << endl;
+    cout << "hits on y-axis: " << hits_y << " misses on y-axis: " << misses_y << endl;
+    cout << "average guess point: " << sum_x/num_data << ", " << sum_y/num_data << endl;
+    cout << "expected point: " << expected.x << ", " << expected.y << endl;
+
     myfile.close();
     return 0;
 }
